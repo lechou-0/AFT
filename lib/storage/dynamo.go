d388e7ae3e39d4edd7eb48910b68220ae6c41f1c
lechou-0 <78336610+lechou-0@users.noreply.github.com>
2022-05-04 19:26:48 +0800

    Update dynamo.go

--- lib/storage/dynamo.go
+++ lib/storage/dynamo.go
@@ -9,7 +9,7 @@ import (
 	awsdynamo "github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/golang/protobuf/proto"
 
-	pb "github.com/vsreekanti/aft/proto/aft"
+	pb "github.com/lechou-0/AFT/proto/aft"
 )
 
 type DynamoStorageManager struct {
