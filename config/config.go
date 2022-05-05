5053fa0f404f7e244b66b3bc207cc9fcf12fc268
lechou-0 <78336610+lechou-0@users.noreply.github.com>
2022-05-05 20:39:14 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ type AftConfig struct {
 
 func ParseConfig() *AftConfig {
 	home := os.Getenv("GOPATH")
-	confPath := filepath.Join(home, "src", "github.com", "vsreekanti", "aft", "config", "aft-config.yml")
+	confPath := filepath.Join(home, "src", "github.com", "lechou-0", "AFT", "config", "aft-config.yml")
 
 	bts, err := ioutil.ReadFile(confPath)
 	if err != nil {
