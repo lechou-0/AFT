74cad6d7054c4e3abc0e4582a803918790979323
lechou-0 <78336610+lechou-0@users.noreply.github.com>
2022-05-05 12:50:39 +0800

    Update redis.go

--- lib/storage/redis.go
+++ lib/storage/redis.go
@@ -21,8 +21,10 @@ func NewRedisStorageManager(address string, password string) *RedisStorageManage
 		DB: 0,
 		PoolSize: 100,
 	})
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	_, err := rc.Ping().Result()
+	_, err := rc.Ping(ctx).Result()
 	if err != nil {
 		fmt.Printf("Unexpected error while connecting to Redis client:\n%v\n", err)
 		os.Exit(1)
@@ -36,13 +38,14 @@ func (redis *RedisStorageManager) StartTransaction(id string) error {
 }
 
 func (redis *RedisStorageManager) CommitTransaction(transaction *pb.TransactionRecord) error {
+	ctx := context.Background()
 	key := fmt.Sprintf(TransactionKey, transaction.Id, transaction.Timestamp)
 	serialized, err := proto.Marshal(transaction)
 	if err != nil {
 		return err
 	}
 
-	return redis.client.Set(key, serialized, 0).Err()
+	return redis.client.Set(ctx, key, serialized, 0).Err()
 }
 
 func (redis *RedisStorageManager) AbortTransaction(transaction *pb.TransactionRecord) error {
@@ -51,9 +54,10 @@ func (redis *RedisStorageManager) AbortTransaction(transaction *pb.TransactionRe
 }
 
 func (redis *RedisStorageManager) Get(key string) (*pb.KeyValuePair, error) {
+	ctx := context.Background()
 	result := &pb.KeyValuePair{}
 
-	val, err := redis.client.Get(key).Result()
+	val, err := redis.client.Get(ctx, key).Result()
 	if err != nil {
 		return result, err
 	}
@@ -63,9 +67,10 @@ func (redis *RedisStorageManager) Get(key string) (*pb.KeyValuePair, error) {
 }
 
 func (redis *RedisStorageManager) GetTransaction(transactionKey string) (*pb.TransactionRecord, error) {
+	ctx := context.Background()
 	result := &pb.TransactionRecord{}
 
-	val, err := redis.client.Get(transactionKey).Result()
+	val, err := redis.client.Get(ctx, transactionKey).Result()
 	if err != nil {
 		return result, err
 	}
@@ -75,10 +80,11 @@ func (redis *RedisStorageManager) GetTransaction(transactionKey string) (*pb.Tra
 }
 
 func (redis *RedisStorageManager) MultiGetTransaction(transactionKeys *[]string) (*[]*pb.TransactionRecord, error) {
+	ctx := context.Background()
 	results := make([]*pb.TransactionRecord, len(*transactionKeys))
 
 	for index, key := range *transactionKeys {
-		txn, err := redis.GetTransaction(key)
+		txn, err := redis.GetTransaction(ctx, key)
 		if err != nil {
 			return &[]*pb.TransactionRecord{}, err
 		}
@@ -90,17 +96,19 @@ func (redis *RedisStorageManager) MultiGetTransaction(transactionKeys *[]string)
 }
 
 func (redis *RedisStorageManager) Put(key string, val *pb.KeyValuePair) error {
+	ctx := context.Background()
 	serialized, err := proto.Marshal(val)
 	if err != nil {
 		return err
 	}
 
-	return redis.client.Set(key, serialized, 0).Err()
+	return redis.client.Set(ctx, key, serialized, 0).Err()
 }
 
 func (redis *RedisStorageManager) MultiPut(data *map[string]*pb.KeyValuePair) error {
+	ctx := context.Background()
 	for key, val := range *data {
-		err := redis.Put(key, val)
+		err := redis.Put(ctx, key, val)
 		if err != nil {
 			return err
 		}
@@ -110,12 +118,14 @@ func (redis *RedisStorageManager) MultiPut(data *map[string]*pb.KeyValuePair) er
 }
 
 func (redis *RedisStorageManager) Delete(key string) error {
-	return redis.client.Del(key).Err()
+	ctx := context.Background()
+	return redis.client.Del(ctx, key).Err()
 }
 
 func (redis *RedisStorageManager) MultiDelete(keys *[]string) error {
+	ctx := context.Background()
 	for _, key := range *keys {
-		err := redis.Delete(key)
+		err := redis.Delete(ctx, key)
 		if err != nil {
 			return err
 		}
@@ -128,15 +138,15 @@ func (redis *RedisStorageManager) List(prefix string) ([]string, error) {
 	result := []string{}
 	redisPrefix := fmt.Sprintf("%s*", prefix)
 	mtx := &sync.Mutex{}
-
-	err := redis.client.ForEachMaster(func(master *rdslib.Client) error {
+	ctx := context.Background()
+	err := redis.client.ForEachMaster(ctx, func(master *rdslib.Client) error {
 		cursor := uint64(0)
 		additionalKeys := true
-
+		ctx1 := context.Background()
 		for additionalKeys {
 			var scanKeys []string
 			var err error
-			scanKeys, cursor, err = master.Scan(cursor, redisPrefix, 100).Result()
+			scanKeys, cursor, err = master.Scan(ctx1, cursor, redisPrefix, 100).Result()
 
 			if err != nil {
 				return err
